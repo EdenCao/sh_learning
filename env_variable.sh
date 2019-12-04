248b20a8d6a0bcdc9e501fb97c9d7c4e29900658
caoyong <caoyong@bytedance.com>
2019-12-04 21:15:17 +0800

    feat: add env_variable

---
+++ env_variable.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Environment variables are variables accessible to any program or script
+# running in current shell session.
+# They are created just like local variables, but using the keyword `export`
+# intead.
+export GLOBAL_VAR="I'm a global variable"
+
+# $HOME: The current user's home directory
+# $PATH: A colon-separated list of directories in which the shell looks for
+# commands
+# $PWD: The current working directory
+# $RANDOM: The integer between 0 and 32767
+# $UID: The numeric, real user ID of the current user.
+# $PS1: The primary propt string.
+# $PS2: The secondary prompt string.
